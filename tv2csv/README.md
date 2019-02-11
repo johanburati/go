3d7ff4a4ed99b663c893c7969a05f56660771d99
Johan Burati <johan.burati@gmail.com>
2019-02-11 14:49:03 +0900

    Add tv2csv

---
+++ tv2csv/README.md
@@ -0,0 +1,5 @@
+# tv2csv
+Convert a m3u playlist of live tv channels for use in kodi to a csv file
+
+## Format of the csv file
+group-title,tvg-name,tvg-ID,tvg-logo,stream
---
+++ tv2csv/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/jamesnetherton/m3u"
+)
+
+// getValues returns the tag values sorted by tag names
+func getValues(itags []m3u.Tag) []string {
+			names := []string{"group-title", "tvg-name", "tvg-ID", "tvg-logo"}
+			
+			// create a map out of track.Tags 
+			tags := make(map[string]string)
+			for _, t := range itags {
+				// replace comma with colon, since comma is the fild delimiter in csv files
+				v := strings.Replace(t.Value, ",", ":", -1)
+				tags[t.Name] = v
+			}
+
+			// extract the values in order
+			values := make([]string, 0, len(names))
+			for _, n := range names {
+				values = append(values, tags[n])
+			}
+			return values
+}
+
+func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s <filename|fileuri>\n", filepath.Base(os.Args[0]))
+	}
+
+	flag.Parse()
+
+	if flag.NArg() > 0 {
+
+		playlist, err := m3u.Parse(flag.Arg(0))
+
+		if err == nil {
+			for _, track := range playlist.Tracks {
+				values := getValues(track.Tags)
+				if len(values) == 0 {
+					continue
+				}
+				values = append(values, track.URI)
+				// track.Name is mess up if there is comma in the title, use tvg-name tag instead
+
+				fmt.Println(strings.Join(values, ","))
+			}	
+		} else {
+			fmt.Println(err)
+		}
+	}
+}
